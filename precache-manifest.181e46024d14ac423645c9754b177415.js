6c96f6702af3de92d75360ea77172759ae9a8740
eLeontev <joonior199@gmail.com>
2020-06-27 17:06:40 +0300

    Updates

---
+++ precache-manifest.181e46024d14ac423645c9754b177415.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f351e66943a611b35cea5ea94c2ff82a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5f6ff32d057a627d5fa3",
+    "url": "./static/css/main.a5ba4402.chunk.css"
+  },
+  {
+    "revision": "affebb4541d08569a4bb",
+    "url": "./static/js/2.9ac31453.chunk.js"
+  },
+  {
+    "revision": "7c76fd6bf040e2ca66cdad5291f58a3d",
+    "url": "./static/js/2.9ac31453.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f6ff32d057a627d5fa3",
+    "url": "./static/js/main.b84e2a66.chunk.js"
+  },
+  {
+    "revision": "600e1c16b9e5715212ed",
+    "url": "./static/js/runtime-main.fba376a2.js"
+  }
+]);
