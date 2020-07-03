8d3e790b231b74d9bfe6e29cec6602b6d7747d76
eleontev <joonior199@gmail.com>
2020-07-03 18:04:04 +0300

    ✨ integrate provider with internet context to make it dynamic 💄 styles: align connection vertical position

--- src/components/dashboard/internet/provider.tsx
+++ src/components/dashboard/internet/provider.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './provider.scss';
 
 import { providerHeaderLabel } from '../../../constants/cards.constants';
 import { InternetProvider } from '../../../models/dashboard.model';
+import { ParamHandlerArguments, updateUptime } from '../../../mock/system-params.mock';
+import { InternetContext } from '../../../context/internet.context';
 
 export type ProviderProps = InternetProvider & {
     connectionDuration: string;
 };
 
+export const InternetConnectionDuration = React.memo(
+    ({ startedTime, value }: ParamHandlerArguments) => {
+        useContext(InternetContext);
+
+        return (
+            <section className="provider-connection-duration">
+                Connected: {updateUptime({ startedTime, value })}
+            </section>
+        );
+    }
+);
 export const ProviderRenderer = ({
     providerLabel,
     providerType,
@@ -20,6 +33,6 @@ export const ProviderRenderer = ({
             {' / '}
             <span className="provider-info_type">{providerType}</span>
         </section>
-        <section className="provider-connection-duration">Connected: {connectionDuration}</section>
+        <InternetConnectionDuration value="" startedTime={connectionDuration} />
     </section>
 );
