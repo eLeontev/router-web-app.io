09fd28eb669de7b13c52add18fc0fe97a70e564a
eleontev <joonior199@gmail.com>
2020-07-03 18:08:32 +0300

    ✨ integrate internet card with internet context to be able to subscribe nested dynamic components on internet context update

--- src/components/dashboard/cards/internet-card.tsx
+++ src/components/dashboard/cards/internet-card.tsx
@@ -5,21 +5,33 @@ import { TrafficRenderer } from '../internet/traffic';
 import { DetailsRenderer } from '../internet/details';
 
 import { CardContent, InternetCardContent } from '../../../models/dashboard.model';
+import { WithInternetContext } from '../../../context/internet.context';
+import { ContextUpdater } from '../internet/context-updater';
 
-export const InternetCardComponent = (cardContent: CardContent) => {
-    const {
-        main: { traffic, startedTime, provider },
-        details,
-    } = cardContent as InternetCardContent;
+export const InternetCardRenderer = (cardContent: InternetCardContent) => {
+    const { main, details } = cardContent;
+    const { trafficInfo, startedTime, provider } = main;
+    const { upload, download } = trafficInfo;
 
     return (
         <section className="card-content">
             <ProviderRenderer {...provider} connectionDuration={startedTime} />
             <section className="traffics">
-                <TrafficRenderer {...traffic.download} />
-                <TrafficRenderer {...traffic.upload} />
+                <TrafficRenderer {...download} />
+                <TrafficRenderer {...upload} />
             </section>
             <DetailsRenderer details={details} />
         </section>
+    );
+};
+export const InternetCardComponent = (cardContent: CardContent) => {
+    return (
+        <WithInternetContext>
+            <ContextUpdater
+                details={(cardContent as InternetCardContent).details}
+                trafficInfo={(cardContent as InternetCardContent).main.trafficInfo}
+            />
+            <InternetCardRenderer {...(cardContent as InternetCardContent)} />
+        </WithInternetContext>
     );
 };
