2e5e247452c5fc50a33c0eb2abe5315b102716cb
eleontev <joonior199@gmail.com>
2020-07-03 18:07:24 +0300

    ✨ integrate with internet context to make traffic dynamic 💄 refactor traffic component to split with its graph

--- src/components/dashboard/internet/traffic.tsx
+++ src/components/dashboard/internet/traffic.tsx
@@ -1,31 +1,36 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './traffic.scss';
 
-import { Traffic, TrafficValue } from '../../../models/dashboard.model';
+import { TrafficGraph } from './traffic-graph';
+
+import { InternetContext } from '../../../context/internet.context';
 import { trafficType, trafficTypeLabel } from '../../../constants/cards.constants';
 
-export type TrafficGraphProps = {
-    max: TrafficValue;
-    current: TrafficValue;
-};
-export const TrafficGraph = ({}: TrafficGraphProps) => (
-    <section className="traffic-graph"></section>
-);
+import { Traffic, TrafficValue } from '../../../models/dashboard.model';
 
 export const getTrafficValue = ({ unit, value }: TrafficValue) => `${value} ${unit}`;
 export const getTrafficModifier = (type: trafficType) => `traffic__${type}`;
 
-export const TrafficRenderer = ({ type, max, current }: Traffic) => {
+export const TrafficRenderer = ({ type, max }: Traffic) => {
+    const { internetState: state } = useContext(InternetContext);
+    const currentTrafficValue = type === trafficType.upload ? state.upload : state.download;
+
     return (
         <section className={`traffic ${getTrafficModifier(type)}`}>
             <section className="traffic-info">
                 <section className="traffic-info_actual">
                     <span className="traffic-info_actual-type">{trafficTypeLabel[type]}: </span>
-                    <span className="traffic-info_actual-value">{getTrafficValue(current)}</span>
+                    <span className="traffic-info_actual-value">
+                        {getTrafficValue(currentTrafficValue)}
+                    </span>
                 </section>
                 <section className="traffic-info_max-speed">{getTrafficValue(max)}</section>
             </section>
-            <TrafficGraph max={max} current={current} />
+            <TrafficGraph
+                max={max}
+                current={currentTrafficValue}
+                isUpload={type === trafficType.upload}
+            />
         </section>
     );
 };
