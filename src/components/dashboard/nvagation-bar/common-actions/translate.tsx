ac3167bc83011262c9b6d01e3bdee9a50d7e7686
eleontev <joonior199@gmail.com>
2020-07-08 16:08:49 +0300

    ✨ implement translate action switcher en/ru

--- src/components/dashboard/nvagation-bar/common-actions/translate.tsx
+++ src/components/dashboard/nvagation-bar/common-actions/translate.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { useSetRecoilState } from 'recoil';
+
+import { i18nState } from '../../../../recoil-state/i18n/i18n.atom';
 
 import { navActionLabels } from '../../../../constants/nav.constants';
 
 import { navActionTypes } from '../../../../models/nav.model';
+import { languages } from '../../../../models/i18n.model';
 
-export const Translate = React.memo(() => (
-    <section className="dropdown-as-link">{navActionLabels[navActionTypes.translate]}</section>
-));
+const { russian, english } = languages;
+export const Translate = React.memo(() => {
+    const setLanguage = useSetRecoilState(i18nState);
+
+    const changeLanguage = useCallback(
+        () => setLanguage((language: languages) => (language === russian ? english : russian)),
+        [setLanguage]
+    );
+    return (
+        <section onClick={changeLanguage} className="dropdown-as-link">
+            {navActionLabels[navActionTypes.translate]}
+        </section>
+    );
+});
