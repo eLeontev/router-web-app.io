40bd2e17d2371c529aab7e797d52792851885872
eleontev <joonior199@gmail.com>
2020-07-09 14:42:19 +0300

    ✨ wrap all app to recoil state to maintain states between routers

--- src/pages/router.tsx
+++ src/pages/router.tsx
@@ -13,22 +13,22 @@ const { NODE_ENV, PUBLIC_URL } = process.env;
 const basename = NODE_ENV === 'development' ? '/' : PUBLIC_URL;
 
 export const AppRouter = () => (
-    <Router basename={basename}>
-        <Switch>
-            <Route path={dashboardPath}>
-                <WithLoaderContext>
-                    <WithModalContext>
-                        <RecoilRoot>
+    <RecoilRoot>
+        <Router basename={basename}>
+            <Switch>
+                <Route path={dashboardPath}>
+                    <WithLoaderContext>
+                        <WithModalContext>
                             <DashboardPage />
-                        </RecoilRoot>
-                    </WithModalContext>
-                </WithLoaderContext>
-            </Route>
-            <Route path={rootPath}>
-                <WithLoaderContext>
-                    <LoginPage />
-                </WithLoaderContext>
-            </Route>
-        </Switch>
-    </Router>
+                        </WithModalContext>
+                    </WithLoaderContext>
+                </Route>
+                <Route path={rootPath}>
+                    <WithLoaderContext>
+                        <LoginPage />
+                    </WithLoaderContext>
+                </Route>
+            </Switch>
+        </Router>
+    </RecoilRoot>
 );
