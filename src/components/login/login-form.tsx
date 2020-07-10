7118fbafd65b04820c01caffc6908be97a466868
eleontev <joonior199@gmail.com>
2020-07-10 12:21:48 +0300

    🔨 split login to different components move context to the component instead of wrapping it in the routing

---
+++ src/components/login/login-form.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+
+import { Button } from '../common/button';
+import { Input } from '../common/input';
+import { Message } from '../common/message';
+
+import { LoginLabels } from '../../services/i18n.login.service';
+
+import { loginActionTypes } from '../../constants/login.constants';
+
+import { LoginState } from '../../models/login.model';
+
+export type LoginFormProps = {
+    state: LoginState;
+    i18nLabels: LoginLabels;
+    dispatch: React.Dispatch<any>;
+};
+
+const { cleanup, cleanuperror, setdefault, validate } = loginActionTypes;
+
+const loginInputName = 'login';
+const passwordInputName = 'password';
+
+export const LoginForm = React.memo(
+    ({ i18nLabels, dispatch, state: { login, password } }: LoginFormProps) => (
+        <section className="login">
+            <h2 className="_aligned">{i18nLabels.loginTitleLabel}</h2>
+            <Button
+                className="hint-button hint-button__left"
+                buttonName={i18nLabels.resetLabel}
+                buttonHandler={() => dispatch({ type: cleanup })}
+            />
+            <Button
+                className="hint-button  hint-button__right"
+                buttonName={i18nLabels.setDefaultLabel}
+                buttonHandler={() => dispatch({ type: setdefault })}
+            />
+            <section className="login-form">
+                <Input
+                    name={loginInputName}
+                    value={login}
+                    onChange={(value: string) => dispatch({ type: loginActionTypes.login, value })}
+                />
+                <Input
+                    name={passwordInputName}
+                    value={password}
+                    onChange={(value: string) =>
+                        dispatch({ type: loginActionTypes.password, value })
+                    }
+                    type="password"
+                />
+                <Button
+                    buttonName={i18nLabels.loginButtonLabel}
+                    buttonHandler={() => dispatch({ type: validate })}
+                />
+                <Message
+                    type="error"
+                    message={i18nLabels.invalidCredentialsMessageLabel}
+                    onClose={() => dispatch({ type: cleanuperror })}
+                />
+            </section>
+        </section>
+    )
+);
--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,9 +1,8 @@
 import React, { useReducer } from 'react';
 import './login.scss';
 
-import { Input } from '../../components/common/input';
-import { Button } from '../../components/common/button';
-import { Message } from '../../components/common/message';
+import { WithLoaderContext } from '../../context/loader.context';
+import { LoginForm } from '../../components/login/login-form';
 
 import { loginReducer } from '../../reducers/login.reducer';
 import { ConcurrencyLoginHandler } from '../../components/concurrency/concurrency-login-handler';
@@ -13,69 +12,30 @@ import { initialState, loginActionTypes } from '../../constants/login.constants'
 
 import { LoginState, LoginReducer } from '../../models/login.model';
 
-const { cleanup, cleanuperror, cleanuplogin, setdefault, validate } = loginActionTypes;
-
-const loginInputName = 'login';
-const passwordInputName = 'password';
+const { cleanuplogin } = loginActionTypes;
 
 export const LoginPage = () => {
-    const [{ login, password, shouldLogin, errorMessage }, dispatch] = useReducer<
-        LoginReducer,
-        LoginState
-    >(loginReducer, initialState, () => initialState);
+    const [state, dispatch] = useReducer<LoginReducer, LoginState>(
+        loginReducer,
+        initialState,
+        () => initialState
+    );
+    const { errorMessage, shouldLogin, password, login } = state;
+
     const i18nLabels = useGetLoginPageLabels(errorMessage);
 
     return (
-        <>
+        <WithLoaderContext>
             <section id="root-portal-modal"></section>
             {shouldLogin ? (
                 <ConcurrencyLoginHandler
                     loginValues={{ login, password }}
-                    errorHandler={(errorMessage: string): undefined => {
-                        dispatch({ type: cleanuplogin, value: errorMessage });
-                        return;
-                    }}
+                    errorHandler={(errorMessage: string) =>
+                        dispatch({ type: cleanuplogin, value: errorMessage })
+                    }
                 />
             ) : null}
-            <section className="login">
-                <h2 className="_aligned">{i18nLabels.loginTitleLabel}</h2>
-                <Button
-                    className="hint-button hint-button__left"
-                    buttonName={i18nLabels.resetLabel}
-                    buttonHandler={() => dispatch({ type: cleanup })}
-                />
-                <Button
-                    className="hint-button  hint-button__right"
-                    buttonName={i18nLabels.setDefaultLabel}
-                    buttonHandler={() => dispatch({ type: setdefault })}
-                />
-                <section className="login-form">
-                    <Input
-                        name={loginInputName}
-                        value={login}
-                        onChange={(value: string) =>
-                            dispatch({ type: loginActionTypes.login, value })
-                        }
-                    />
-                    <Input
-                        name={passwordInputName}
-                        value={password}
-                        onChange={(value: string) =>
-                            dispatch({ type: loginActionTypes.password, value })
-                        }
-                        type="password"
-                    />
-                    <Button
-                        buttonName={i18nLabels.loginButtonLabel}
-                        buttonHandler={() => dispatch({ type: validate })}
-                    />
-                    <Message
-                        type="error"
-                        message={i18nLabels.invalidCredentialsMessageLabel}
-                        onClose={() => dispatch({ type: cleanuperror })}
-                    />
-                </section>
-            </section>
-        </>
+            <LoginForm dispatch={dispatch} i18nLabels={i18nLabels} state={state} />
+        </WithLoaderContext>
     );
 };
