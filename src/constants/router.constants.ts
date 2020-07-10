48262660de331c012c6095f4395175c00fde6745
eleontev <joonior199@gmail.com>
2020-07-10 15:59:48 +0300

    ✨ add and integrate simple devices list component

--- src/constants/router.constants.ts
+++ src/constants/router.constants.ts
@@ -1,7 +1,10 @@
-import { DashboardPage } from '../pages/dashboard/dashboard';
-import { IsNotImplemented } from '../pages/is-not-implemented';
-
 import { ComponentsToRender } from '../models/router.model';
+
+import { navActionLinks } from './nav.constants';
+
+import { IsNotImplemented } from '../pages/is-not-implemented';
+import { DashboardPage } from '../pages/dashboard/dashboard';
+import { DevicesList } from '../pages/devices-list/devices-list';
 
 export const rootPath = '/';
 export const dashboardPath = '/dashboard';
@@ -10,4 +13,5 @@ export const rootControlPanelPath = '/controlpanel';
 export const componentsToRender: ComponentsToRender = {
     default: IsNotImplemented,
     [dashboardPath]: DashboardPage,
+    [navActionLinks.deviceList]: DevicesList,
 };
---
+++ src/pages/devices-list/devices-list.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const DevicesList = () => {
+    return <h1>Devices list</h1>;
+};
